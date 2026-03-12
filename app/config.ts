de43ca690c6c27af639197a62abf1bef82f0c1db
haichudo <123709389+haichudo@users.noreply.github.com>
2026-03-12 11:14:14 +0700

    Fix whitespace in siteName configuration

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  siteName: process.env.SITE_NAME || "Gia phả họ Chu - Thôn Khúc Lộng - Xã Nghĩa Trụ (Văn Giang) - Hưng yên",
+  siteName: process.env.SITE_NAME || "Gia phả họ Chu - Thôn Khúc Lộng - Xã Nghĩa Trụ    (Văn Giang) - Hưng yên",
   exampleEmail: process.env.EXAMPLE_EMAIL || "",
   examplePassword: process.env.EXAMPLE_PASSWORD || "",
   demoDomain: process.env.DEMO_DOMAIN || "https://giaphahochu.vercel.app/dashboard",
