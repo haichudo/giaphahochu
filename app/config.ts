827be3cae1014b103265dd7346d8827b6d7b5a1c
haichudo <123709389+haichudo@users.noreply.github.com>
2026-03-12 09:23:31 +0700

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  siteName: process.env.SITE_NAME || "Gia phả họ Chu - Nghĩa Trụ (Văn Giang cũ) - Hưng Yên",
+  siteName: process.env.SITE_NAME || "Gia phả họ Chu - Nghĩa Trụ - Hưng yên (xã Văn Giang cũ)",
   exampleEmail: process.env.EXAMPLE_EMAIL || "",
   examplePassword: process.env.EXAMPLE_PASSWORD || "",
   demoDomain: process.env.DEMO_DOMAIN || "https://giaphahochu.vercel.app/dashboard",
