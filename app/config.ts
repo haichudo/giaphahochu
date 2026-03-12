affbc9bbe7a591e1f08110e123263749378b3aa0
haichudo <123709389+haichudo@users.noreply.github.com>
2026-03-12 09:24:01 +0700

    Fix formatting of siteName in config

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,6 @@
 const config = {
-  siteName: process.env.SITE_NAME || "Gia phả họ Chu - Nghĩa Trụ - Hưng yên (xã Văn Giang cũ)",
+  siteName: process.env.SITE_NAME || "Gia phả họ Chu - Nghĩa Trụ - Hưng yên 
+    (xã Văn Giang cũ)",
   exampleEmail: process.env.EXAMPLE_EMAIL || "",
   examplePassword: process.env.EXAMPLE_PASSWORD || "",
   demoDomain: process.env.DEMO_DOMAIN || "https://giaphahochu.vercel.app/dashboard",
