ca2ab04b4bed97151a38fccd8ab1ae6932eaa7b5
haichudo <123709389+haichudo@users.noreply.github.com>
2026-03-13 09:06:31 +0700

    Fix site name casing and update location details

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,7 @@
 const config = {
   siteName:
     process.env.SITE_NAME ||
-    `Gia phả họ Chu
+    `Gia phả họ CHU
 - Thôn Khúc Lộng - Xã Nghĩa Trụ - Tỉnh Hưng Yên (Xưa là Xóm Đọ-Thôn Vĩnh Khúc-Mỹ Văn-Văn Giang-Tỉnh Hưng Yên `,
   exampleEmail: process.env.EXAMPLE_EMAIL || "",
   examplePassword: process.env.EXAMPLE_PASSWORD || "",
