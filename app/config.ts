95652fafce63e335a32666b0652772928d587bee
haichudo <123709389+haichudo@users.noreply.github.com>
2026-03-13 09:31:58 +0700

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,8 +1,7 @@
 const config = {
   siteName:
     process.env.SITE_NAME ||
-    `Gia phả họ CHU
-- Thôn Khúc Lộng - Xã Nghĩa Trụ - Tỉnh Hưng Yên (Xưa là Xóm Đọ-Thôn Vĩnh Khúc-Mỹ Văn-Văn Giang-Tỉnh Hưng Yên `,
+    `Gia phả họ CHU`,
   exampleEmail: process.env.EXAMPLE_EMAIL || "",
   examplePassword: process.env.EXAMPLE_PASSWORD || "",
   demoDomain:
